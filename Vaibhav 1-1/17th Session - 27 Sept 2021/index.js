b917ddb269cff5e1499e6d6eee4d51ea0a492043
Akshay Khurana <akhurana@sdgecontractor.com>
2021-09-27 19:59:07 +0530

    17th Session Initial Commit

---
+++ Vaibhav 1-1/17th Session - 27 Sept 2021/index.js
@@ -0,0 +1,13 @@
+/*
+    Today's Topics
+    
+    ES6 Features & Syntax
+    Arrow Function
+    JavaScript Events
+    Map, Filter, Reduce
+    this Keyword
+    Form Validation & Data Submission
+    Manipulating Style, Text, Content, Attributes, Events
+    DOM methods - createElement, setAttribute, appendChild, append
+
+*/
