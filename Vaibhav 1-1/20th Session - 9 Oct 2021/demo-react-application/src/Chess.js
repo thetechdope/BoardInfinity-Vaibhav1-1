1a215418042cc6ff42758960fc131bb23b96d788
Akshay Khurana <akhurana@sdgecontractor.com>
2021-10-08 21:13:20 +0530

    React First Application

---
+++ Vaibhav 1-1/20th Session - 9 Oct 2021/demo-react-application/src/Chess.js
@@ -0,0 +1,86 @@
+import './Chess.css';
+import Black from './Black';
+import White from './White';
+
+/* Functional Component */
+
+function Chess() {
+    return (
+        <div className="main-class">
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <br />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <br />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <br />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <br />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <br />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <br />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <br />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <White />
+            <Black />
+            <br />
+        </div>
+    );
+}
+
+export default Chess;
---
+++ Vaibhav 1-1/20th Session - 9 Oct 2021/demo-react-application/src/White.js
@@ -0,0 +1,13 @@
+import './Chess.css';
+
+/* Functional Component */
+
+function White() {
+    return (
+        <div className="white">
+            Akshay
+        </div>
+    );
+}
+
+export default White;
